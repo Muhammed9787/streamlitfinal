613d6829e44decde3546c6875d2f6e6ec976b261
Muhammed9787 <79834550+Muhammed9787@users.noreply.github.com>
2021-12-07 12:16:28 +0200

    save

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ from PIL import  Image
 
 # Custom imports 
 from multipage import MultiPage
-from pages import data_upload, machine_learning, metadata, data_visualize, redundant # import your pages here
+from pages import data_upload, machine_learning, metadata, data_visualize, redundant ,arima_sir,logistic_model mo# import your pages here
 
 # Create an instance of the app 
 app = MultiPage()
@@ -25,6 +25,8 @@ app.add_page("Change Metadata", metadata.app)
 app.add_page("Machine Learning", machine_learning.app)
 app.add_page("Data Analysis",data_visualize.app)
 app.add_page("Y-Parameter Optimization",redundant.app)
+app.add_page("Y-Parameter Optimization",logistic_model.app)
+app.add_page("Y-Parameter Optimization",arima_sir.app)
 
 # The main app
 app.run()
